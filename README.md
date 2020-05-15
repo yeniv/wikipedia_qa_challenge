aabd5c9f5dc10ea14ed1cb4d55d0e97e93c3b71c
yeniv <tristanviney@gmail.com>
2020-05-15 16:40:34 +0200

    challenge set for future POP QA

---
+++ README.md
@@ -0,0 +1,26 @@
+# POP Wikipedia QA Challenge 
+
+Hi there 👋
+
+Firstly, thank you for taking the time to tackle this coding challenge. We’re all very excited to see how you approach the problems we’ve set out for you. 💪
+
+If you haven’t used Cypress before, don’t worry. You can check out Cypress' thorough and really helpful documentation [here](https://docs.cypress.io/) and we've also left the example tests in the repository `cypress/integration/examples` to help guide you. 
+
+There are 9 challenges in `cypress/integration/wikipedia_qa_challenge.js` which test some of Wikipedia's functionality. The challenges have been written as comments in the file. We have completed the first 3, and would like you to complete as many of the remaining 6 as you can. 
+
+If you have any questions, please contact [David Sigley](mailto:david@iampop.com) or [myself](tristan@iampop.com). 
+
+## Steps to load test locally:
+1. Clone this repository `git clone https://github.com/yeniv/qa_wikipedia_challenge.git`
+2. Run `yarn` from your command line interface to install the cypress dependancy
+3. Run `yarn run cypress open` to open the Cypress app (run each time as you update your specs, so stop/start Cypress as necessary)
+4. Open and complete the challenges in ‘cypress/integration/wikipedia_qa_challenge.js’ 
+
+## Test login credentials:
+- Username: `tristanfrompop`
+- Password: `hello.potential.teammate!`
+
+## Once completed:
+Please push your updated version of the repository to your GitHub account and share the link with [David Sigley](mailto:david@iampop.com). 
+
+Good luck 🎉
---
+++ cypress/integration/wikipedia_qa_challenge.js
@@ -0,0 +1,57 @@
+describe('Wikipedia functionality', () => {
+
+  beforeEach(() => {
+    cy.visit('https://en.wikipedia.org')
+  })
+
+  after(() => {
+    cy.visit('https://en.wikipedia.org/wiki/Special:UserLogout')
+  
+    cy.get('button.oo-ui-inputWidget-input.oo-ui-buttonElement-button')
+      .click()
+  })
+
+  // CHALLENGE 1: ensure the correct homepage welcome message is displayed
+  it('Hompage welcome message', () => {
+    cy.get('div').contains('Welcome to Wikipedia')
+  })
+
+  // // CHALLENGE 2: ensure the "Today's featured picture" section actually contains a picture
+  it('Today\'s featured picture contains a picture', () => {
+    cy.get('div#mp-bottom').find('img')
+  }) 
+  
+  // CHALLENGE 3: ensure the main navigation links display correctly
+  it('Main navigation links', () => {
+    cy.get('div#p-navigation').find('li').then((links) => {
+      cy.wrap(links[0]).should('contain', 'Main page')
+      cy.wrap(links[1]).should('contain', 'Contents')
+      cy.wrap(links[2]).should('contain', 'Featured content')
+      cy.wrap(links[3]).should('contain', 'Current events')
+      cy.wrap(links[4]).should('contain', 'Random article')
+      cy.wrap(links[5]).should('contain', 'Donate to Wikipedia')
+      cy.wrap(links[6]).should('contain', 'Wikipedia store')
+    })
+  })
+
+  // // CHALLENGE 4: Test unsuccessful login attempt scenarios
+  it('Handle unsuccessful login attempts', () => {
+    // TODO: solution to go here
+  })
+
+  // CHALLENGE 5: ensure you can log in with provided credientials 
+  it('Log successfully', () => {
+    // TODO: solution to go here
+  })
+
+  // CHALLENGE 6: ensure you can successfully search for 'Bill Murray' in the search bar and the correct information is returned
+  it('Successful Bill Murray search', () => {
+    // TODO: solution to go here
+  })
+  
+  // CHALLENGE 7: make the login steps required for challenge 4 reusable by writing them as a Cypress command (support/commands.js)
+  //              use this new command in your solution for CHALLENGE 5
+  
+  // CHALLENGE 8: Improve the after hook so that the 'logout' button is only clicked if it actually exists 
+  //              (so we only try to click the button if Cyress is logged in)
+})
