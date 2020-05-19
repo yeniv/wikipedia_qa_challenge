c1891ce3382d0f2f10c9b0c18b42da65f265c160
jelwij <65549919+jelwij@users.noreply.github.com>
2020-05-19 16:38:21 +0200

    Challenge completed
    updated wikipedia_qa_challange.js

--- cypress/integration/wikipedia_qa_challenge.js
+++ cypress/integration/wikipedia_qa_challenge.js
@@ -5,11 +5,17 @@ describe('Wikipedia functionality', () => {
   })
 
   after(() => {
+
+    if (cy.get('li#pt-logout').should('have.text', 'Log out')) {
+
     cy.visit('https://en.wikipedia.org/wiki/Special:UserLogout')
   
     cy.get('button.oo-ui-inputWidget-input.oo-ui-buttonElement-button')
       .click()
+}
   })
+
+	
 
   // CHALLENGE 1: ensure the correct homepage welcome message is displayed
   it('Hompage welcome message', () => {
@@ -34,23 +40,49 @@ describe('Wikipedia functionality', () => {
     })
   })
 
+Cypress.Commands.add('typeLogin', (user) => {
+  cy.get('li#pt-login').click()
+
+  cy.get('input#wpName1')
+    .type(user.name)
+
+  cy.get('input#wpPassword1')
+    .type(user.password)
+
+  cy.get('button#wpLoginAttempt').click()
+})
+
   // CHALLENGE 4: ensure unsuccessful login attempt scenarios are handled correctly
   it('Handle unsuccessful login attempts', () => {
-  // TODO: solution to go here
+   
+                cy.typeLogin({ name: 'WRONG!', password: 'ALSO_WRONG!' })
+
+		cy.get('.errorbox').contains('Incorrect username or password entered. Please try again.')
+		
+
   })
 
   // CHALLENGE 5: ensure you can log in with provided credientials successfully
   it('Log successfully', () => {
-  // TODO: solution to go here
+                cy.typeLogin({ name: 'tristanfrompop', password: 'hello.potential.teammate!' })
+		
+		cy.get('li#pt-logout').should('have.text', 'Log out')
+		cy.get('li#pt-userpage').should('have.text', 'Tristanfrompop')
+
   })
 
   // CHALLENGE 6: ensure you can successfully search for 'Bill Murray' in the search bar and the correct information is returned
   it('Successful Bill Murray search', () => {
-  // TODO: solution to go here
+
+		cy.get('input#searchInput').type('Bill Murray', { delay: 100 })
+		cy.get('input#searchButton').click()
+		cy.url().should('include', '/Bill_Murray')
+ 
   })
   
   // CHALLENGE 7: make the login steps required for challenge 4 reusable by writing them as a Cypress command (support/commands.js)
   //              use this new command in your solution for CHALLENGE 5
+
   
   // CHALLENGE 8: improve the after hook so that the 'logout' button is only clicked if it actually exists 
   //              (so we only try to click the button if Cyress is logged in)
